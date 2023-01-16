5300a1b72db587e5d7a32d044b2979599c3dcaa4
leonigmig <leonigmig@gmail.com>
2023-01-16 11:37:35 +0000

    functional approach suggested by gpt

--- README.rst
+++ README.rst
@@ -2,3 +2,5 @@ Simple sequencer
 ----------------
 
 Just a simple event based sequencer
+
+lets say I want a function that acts as a player which takes a song as a parameter and is responsible for sending midi notes, keeping track of time and sleeping between notes in the song. the song knows which notes to play at any identified tick and is responsible for dynamically repeating, muting and varying the patterns according to a structure, pitch space and motive rhythm it takes as parameters. the patterns can be either pitch patterns or rhythm patterns. the whole structure is immutable. what pure python patterns can i use to compose this software
---
+++ src/func_sugg.py
@@ -0,0 +1,43 @@
+import time
+# from functools import partial, reduce
+
+
+def play_song(song_func, state, tick=0):
+    """
+    A higher-order function that plays the song by calling the song_func with
+    the current tick and state
+    """
+    if tick >= state["end_tick"]:
+        return
+    note, new_state = song_func(tick, state)
+    print("tick:", tick)
+    print("Playing note:", note)
+    time.sleep(note["duration"])
+    play_song(song_func, new_state, tick + 1)
+
+
+def song(tick, state):
+    """
+    A recursive function that represents the song. It takes a current tick and
+     state as parameters and returns the next note to play
+    """
+    if tick >= state["end_tick"]:
+        return None
+
+    current_note = state["notes"][tick % len(state["notes"])]
+    new_state = {"end_tick": state["end_tick"], "notes": state["notes"]}
+    return {"pitch": current_note["pitch"],
+            "duration": current_note["duration"]}, new_state
+
+
+# Define the notes and structure of the song
+notes = [
+    {"pitch": 60, "duration": 0.5},
+    {"pitch": 62, "duration": 0.5},
+]
+
+# song_state = {"end_tick": float("inf"), "notes": notes}
+song_state = {"end_tick": 4, "notes": notes}
+
+# play the song
+play_song(song, song_state)
