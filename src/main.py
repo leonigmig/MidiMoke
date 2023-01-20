191ffb573c774fdfeb9df4aa947e6b2cc65b63f5
leonigmig <leonigmig@gmail.com>
2023-01-20 18:38:49 +0000

    cleanup and semantics

--- src/main.py
+++ src/main.py
@@ -1,39 +1,63 @@
+import sys
+
+from rich import print
+import climax
+
 from midisync import Tempo
 from movement import make_movement
 from pattern import Pattern
 from midiport import MidiPort
 from player import Player
 
-import climax
-from rich import print
+
+def make_test_pattern():
+    pattern = Pattern()
+    pattern.addNote(
+        pitch=60, tick=1, duration=1, volume=100)
+    pattern.addNote(
+        pitch=60, tick=2, duration=1, volume=100)
+    pattern.addNote(
+        pitch=64, tick=1, duration=1, volume=100)
+    pattern.addNote(
+        pitch=64, tick=2, duration=2, volume=100)
+    pattern.addNote(
+        pitch=72, tick=3, duration=1, volume=100)
+    return pattern
+
+
+def play_at_this_speed_to_this_midibus(bpm, out):
+    """Core time loop for playing a movement."""
+
+    sys.setrecursionlimit(2000)
+
+    movement = make_movement(make_test_pattern(), 3)
+    try:
+        player = Player(
+            bpm,
+            movement,
+            out
+        )
+
+        player.play(0)
+
+    except RecursionError as e:
+        print(e)
+        print("That's enough of that!")
 
 
 @climax.command()
-@climax.argument('sync_port', help='the sync port name to bind to')
-@climax.argument('out_port', help='the out port name to bind to')
-def initialise_event_loop(sync_port, out_port):
-    print("[italic blue]Running[/italic blue] 🦋✨")
-    print(f"Sync and control port: {sync_port}")
-    print(f"Output port: {out_port}")
+@climax.argument('midi_in', help='transport, sync, event midi port to bind to')
+@climax.argument('midi_out', help='primary midi output bus for notes, events')
+def initialise_event_loop(midi_in, midi_out):
+    """Connect to MIDI, prepare access to device hardware clock,
+    load movement and app settings from configuration then play."""
 
-    tempo = Tempo(120)
-    out_port = MidiPort(out_port)
+    print("[italic blue]Blue Butterly alphav0.1[/italic blue] 🦋✨")
 
-    pattern_1 = Pattern()
-    pattern_1.addNote(
-        pitch=60, tick=1, duration=1, volume=100)
-    pattern_1.addNote(
-        pitch=60, tick=2, duration=1, volume=100)
-    pattern_1.addNote(
-        pitch=64, tick=1, duration=1, volume=100)
-    pattern_1.addNote(
-        pitch=64, tick=2, duration=2, volume=100)
+    bpm = Tempo(120)
+    out = MidiPort(midi_out)
 
-    movement = make_movement(pattern_1, 3)
-
-    player = Player(tempo, movement, out_port)
-
-    player.play(0)
+    play_at_this_speed_to_this_midibus(bpm, out)
 
 
 if __name__ == '__main__':
