0a24208844ee56faa8a0dd6a2beefcf469516912
leonigmig <leonigmig@gmail.com>
2023-04-09 17:23:42 +0100

    working with m21, but still not syncing right

--- src/m21.py
+++ src/m21.py
@@ -1,6 +1,6 @@
 from music21 import stream, note
 from music21 import midi
-from music21 import corpus
+from music21 import converter, meter, key
 
 
 def convert_music21_stream(stream):
@@ -35,5 +35,17 @@ def get_stream():
         arpeggio_note.duration.quarterLength = 1  # Set the note duration to 1 quarter note
         arpeggio.append(arpeggio_note)
 
-    fugue = corpus.parse('bwv66.6')
-    return fugue
+    melodic_techno_lead = '''
+    C4. C4 E-4 G4 C5 G4 E-4 C4
+    G3. G3 B-3 D4 G4 D4 B-3 G3
+    A3. A3 C4 E-4 A4 E-4 C4 A3
+    E-3. E-3 G3 B-3 E-4 B-3 G3 E-3
+    '''
+
+    lead_line = converter.parse("tinyNotation: " + melodic_techno_lead)
+
+    lead_line.insert(0, meter.TimeSignature('4/4'))
+    lead_line.insert(0, key.KeySignature(-3))
+
+    lead_line.repeatAppend(lead_line, numberOfTimes=2)
+    return lead_line
